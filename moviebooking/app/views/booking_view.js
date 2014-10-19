05d6c45726e67afb8f065a964da603acf9535926
Nicholas Teo <nicholasteock@gmail.com>
2014-10-19 20:03:52 +0800

    Finished up initial skeleton of site

--- moviebooking/app/views/booking_view.js
+++ moviebooking/app/views/booking_view.js
@@ -1,13 +1,93 @@
-var View     = require('./view')
-  , template = require('./templates/booking');
+var View     = require('./view'),
+	template = require('./templates/booking');
 
-
+var events = {
+	'click .seat' 	: 'seatClicked'
+};
 
 var afterRender = function() {
+	setTimeout( function() {
+		//case I: Show from starting
+		//init();
+
+		//Case II: If already booked
+		// var bookedSeats = [5, 10, 25];
+		// init(bookedSeats);
+		init([ "A-1", "B-10", "C-5" ]);
+	}, 100);
+};
+
+var settings = {
+   rows: 11,
+   cols: 20,
+   rowCssPrefix: 'row-',
+   colCssPrefix: 'col-',
+   seatWidth: 35,
+   seatHeight: 35,
+   seatCss: 'seat',
+   selectedSeatCss: 'selectedSeat',
+   selectingSeatCss: 'selectingSeat'
+};
+
+var rowArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+
+var init = function (reservedSeat) {
+    var str = [], seatNo, className, rowChar;
+
+    for( var i=0, iLen=settings.rows; i<iLen; i++) {
+    	rowChar = rowArr[i];
+
+        for (var j = 0, jLen=settings.cols; j<jLen; j++) {
+            // seatNo = (i + j * settings.rows + 1);
+            // className = settings.seatCss + ' ' + settings.rowCssPrefix + i.toString() + ' ' + settings.colCssPrefix + j.toString();
+            seatNo = j + 1;
+            className = settings.seatCss + ' ' + settings.rowCssPrefix + rowChar + ' ' + settings.colCssPrefix + seatNo.toString();
+            
+            if( $.isArray(reservedSeat)  )
+            if ($.isArray(reservedSeat) && $.inArray(rowChar + "-" + seatNo, reservedSeat) != -1) {
+                className += ' ' + settings.selectedSeatCss;
+            }
+            
+            str.push('<li class="' + className + '"' +
+                      'style="top:' + (i * settings.seatHeight).toString() + 'px;left:' + (j * settings.seatWidth).toString() + 'px">' +
+                      '<a title="' + seatNo + '">' + seatNo + '</a>' +
+                      '</li>');
+        }
+    }
+    $('#place').html(str.join(''));
+
+    var seatHtmlArray = [], rowStr = "", seatNo, className;
+};
+
+var seatClicked = function( ev ) {
+	var $seat = $(ev.target),
+		targetClasses = "",
+		seatRow = "",
+		seatCol = "",
+		charPos,
+		spacePos;
+
+	if( $seat.hasClass("selectedSeat") ) { return; }
+
+	$seat.toggleClass( "selectingSeat" );
+	targetClasses = $seat.attr("class");
+	
+	// Get row character
+	charPos = targetClasses.indexOf("row-");
+	spacePos = targetClasses.indexOf(" ", charPos);
+	seatRow = targetClasses.substring( charPos+4, spacePos );
+
+	// Get column number
+	charPos = targetClasses.indexOf("col-");
+	spacePos = targetClasses.indexOf(" ", charPos);
+	if( spacePos === -1 ) { spacePos = targetClasses.length; }
+	seatCol = targetClasses.substring( charPos+4, spacePos );
 };
 
 module.exports = View.extend({
     id: 'booking-view',
     afterRender: afterRender,
-    template: template
+    template: template,
+    events: events,
+    seatClicked: seatClicked
 })
