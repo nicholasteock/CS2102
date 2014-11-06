bb63070cd521e4a8ddad4dd36e7b87f395210878
nicholasteock <nicholasteock@gmail.com>
2014-11-07 03:36:09 +0800

    .

---
+++ moviebooking/app/views/edit_view.js
@@ -0,0 +1,58 @@
+var View     = require('./view'),
+	template = require('./templates/edit')
+
+var getRenderData = function() {
+	if(localStorage.userId == undefined || localStorage.name == undefined) {
+		alert("Please log in to continue");
+		Application.router.navigate('login', {trigger: true});
+		return false;
+	}
+
+	var	dfdResult = $.Deferred();
+
+	var onSuccess = function( response ) {
+		return dfdResult.resolve( response );
+	};
+	
+	var onError = function( response ) {
+		return dfdResult.reject( response );
+	};
+
+	var data = {
+		userId: localStorage.userId
+	};
+
+	$.ajax({
+			url 		: Application.api+"bookings",
+			type 		: "POST",
+			dataType	: 'json',
+			data 		: data,
+			success		: onSuccess,
+			error		: onError
+	});
+	
+	return dfdResult;
+};
+
+var logout = function() {
+	localStorage.removeItem('userId');
+	localStorage.removeItem('name');
+	localStorage.removeItem('booking');
+	Application.router.navigate('login', {trigger: true});
+	return false;
+};
+
+var afterRender = function(){
+	$(".logout").click(logout);
+};
+
+var events = {
+};
+
+module.exports = View.extend({
+    id 				: 'edit-view',
+    events 			: events,
+    getRenderData 	: getRenderData,
+    afterRender 	: afterRender,
+    template 		: template
+});
